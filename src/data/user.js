4257ee4460eb5dfd0da3ca7a379c8a818308fa90
Nikhil Dongre <91336150+NikhilDongre1@users.noreply.github.com>
2024-07-31 22:32:49 +0530

    Add files via upload

---
+++ src/data/user.js
@@ -0,0 +1,64 @@
+const INFO = {
+	main: {
+		title: "Portfolio.Nikhil",
+		name: "Nikhil Dongre",
+		email: "nikhildongre8109@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://x.com/NikhilD66925004",
+		github: "https://github.com/NikhilDongre1",
+		linkedin: "https://linkedin.com/in/nikhil-dongre81",
+		instagram: "https://www.instagram.com/_nikhil_dongre/",
+	},
+
+	homepage: {
+		title: "Fullstack Developer and UX Designer",
+		description:
+			"I am a fullstack developer with expertise in ReactJS, Node.js, and Material UI. I have experience in developing and maintaining web applications, improving functionality and responsiveness. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Nikhil Dongre. I live in India, where I design the future.",
+		description:
+			"I've gained hands-on experience in developing, implementing, and maintaining internal web applications. Over the years, I've worked on various projects, including a cryptocurrency tracker, a railway utility website, and a dynamic news platform. Many of these projects are open-source, and I'm always open to collaboration and feedback. My goal is to create impactful solutions that push the boundaries of what's possible.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "CRYPT-ON - A Cryptocurrency Tracker Application",
+			description:
+				"Built a real-time cryptocurrency tracking platform with interactive charts. Implemented detailed coin info pages with current prices and market data. Designed a responsive interface for seamless desktop and mobile use. Tech: React.js, Material UI, Chart.js.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/NikhilDongre1/Crypto-Currency-Dashboard-React",
+		},
+
+		{
+			title: "Railway Website - A Railways Utility Website for Travelers",
+			description:
+				"Constructed an application to Find Trains, Check PNRs, and Track Trains. Developed features for the application and integrated APIs for functionalities. Utilized Node and Express Framework to make the dynamic. Tech: HTML, CSS, JavaScript, Bootstrap, Node, Express.js.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/NikhilDongre1/RailSathi",
+		},
+
+		{
+			title: "News Express - Your Window to World News",
+			description:
+				"Used API integration for fetching top headlines by country. Created a dynamic platform covering diverse topics like technology, business, and health. Implemented global news search for comprehensive regional coverage. Tech: EJS, CSS, JavaScript, Bootstrap, and Express.js.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://news-app-express-nikhil.onrender.com",
+		},
+	],
+};
+
+export default INFO;
